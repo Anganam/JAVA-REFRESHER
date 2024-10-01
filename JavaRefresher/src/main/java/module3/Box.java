c669d1e68088706476defc65baadf13aae5838bd
Anganam <anganamnkili@gmail.com>
2024-10-01 17:37:00 +0200

    module 3 completed

--- JavaRefresher/src/main/java/module3/Box.java
+++ JavaRefresher/src/main/java/module3/Box.java
@@ -9,5 +9,25 @@ package module3;
  * @author Anganam
  */
 public class Box {
+    private Thing[] things = {
+        new Thing(), new Thing(), new Thing()
+    };
     
+    public Thing getOneThing(){
+        return things[2];
+    }
+    
+    public Thing[] getManyThings(){
+        return things;   
+    }
+    
+    public void showThings(){
+        System.out.println("Things");
+        for (Thing t: things){
+            System.out.println(t.toString());
+        }
+        
+        System.out.println("------------------------------------------------");
+    
+    }
 }
--- JavaRefresher/src/main/java/module3/Main.java
+++ JavaRefresher/src/main/java/module3/Main.java
@@ -9,5 +9,25 @@ package module3;
  * @author Anganam
  */
 public class Main {
-    
+    public static void main(String[] args){
+        Thing t = new Thing();
+        //int n = t.numOne;
+        //System.out.println("value read is " + n);
+        
+        
+        /*Box b = new Box();
+        System.out.println("numOne in boxed thing is " + b.getOneThing().numOne);
+        b.getOneThing().numOne = 5432;
+        System.out.println("numOne in boxed thing is " + b.getOneThing().numOne);
+        */
+        
+        /*
+        System.out.println("t is " + t);
+        t.doStuff();
+        System.out.println("t is " + t);
+        */
+        
+        System.out.println("t is the same reference as \"this\"" + " inside a method called on t?" +t.sameAs(t));
+        
+    }
 }
